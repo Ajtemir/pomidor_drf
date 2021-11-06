a0378850743f423bd209bd449db410d916305007
aitemir <ajtemirulanbekuulu@gmail.com>
2021-11-06 18:38:45 +0600

    first commit

---
+++ books/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from books.models import Book
+from books.serializers import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
